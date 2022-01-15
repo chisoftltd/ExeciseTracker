91ea42cd4be93b896c103fb9ff91a5b027edfe5d
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-15 16:58:01 +0000

    Exercise details have been added and now saved to Google sheet. Also all api and key values are in environ variable.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import requests
 
@@ -8,16 +9,38 @@ headers = {
 }
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/a99841fe556d00090b02e62e647eca8c/workoutSheet/workouts"
 exercise_text = input("Tell me which exercise you did? ")
 
 parameters = {
     "query" : exercise_text,
     "gender": os.environ.get("GENDER"),
-    "weight": os.environ.get("WEIGHT"),
-    "height": os.environ.get("HEIGHT"),
+    "weight_kg": os.environ.get("WEIGHT"),
+    "height_cm": os.environ.get("HEIGHT"),
     "age": os.environ.get("AGE"),
 }
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
+
+#   ====================== Start of Sheet API Mechanism =================================
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result['exercises']:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+    print(sheet_response.text)
+
+
 print(result)
